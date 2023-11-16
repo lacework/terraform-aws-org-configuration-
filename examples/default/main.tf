8d3839db38c6b82622c0fcabdbfac5a9ca1aeb22
Salim Afiune Maya <afiune@lacework.net>
2023-11-15 16:14:29 -0800

    ci: fix example org_unit_id
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -10,6 +10,6 @@ module "aws_org_configuration" {
   lacework_access_key_id = "accesskey"
   lacework_secret_key    = "_secretkey"
   organization_id        = "o-organizationid"
-  organization_unit      = "org-unit"
+  organization_unit      = ["org-unit"]
   cf_resource_prefix     = "prefix"
 }
